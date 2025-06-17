21c53c1f7de3399856f441899a85a7badde66030
Julie-Blondeel <sonya.ivory.art@gmail.com>
2025-06-17 18:50:11 +0200

    Added basic form handing and added Typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert("Hello World");
+function generateRecipe(event) {
+    event.preventDefault();
+
+    new Typewriter("#recipe", {
+        strings: "The cocnut balls",
+        autoStart: true,
+        delay: 1,
+        cursor:"",
+    });
+}
+
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
